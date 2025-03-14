3b11771bd94821609c51de684a35330348d18295
Nakul Ashish Pant <nakul.paradox@gmail.com>
2025-03-14 06:55:16 +0530

    externalized port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 5678;
 const TARGET_URL = process.env.CUSTOM_URL;
 const cors = require('cors');
 
